250e4f735770e6a583fab4fca50cfdc917e96981
Taiwo Ajayi <ajayitaiwo89@gmail.com>
2020-02-23 00:04:45 +0100

    unit test updated

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 var expect = require("chai").expect;
 var app = require("../server");
 var request = require("request");
+var should = require("should");
+var supertest = require("supertest");
 
 //let's set up the data we need to pass to the login method
+
+var server = supertest.agent("http://localhost:4000/api/v1");
 
 describe("Authentication", function() {
   describe("It should save users record", function() {
@@ -12,12 +16,14 @@ describe("Authentication", function() {
         email: "ajayitaiwo89@gmail.com",
         password: "12345678"
       };
-      var authenticatedUser = request.agent(app);
-      authenticatedUser
+      // var authenticatedUser = request.agent(app);
+      server
         .post("/signup")
+        .expect("Content-type", /text/)
+        .expect(200)
         .send(userCredentials)
         .end(function(error, response) {
-          expect(response.statusCode).to.equal(201);
+          expect(response.statusCode).to.equal(200);
           done();
         });
     });
@@ -27,9 +33,10 @@ describe("Authentication", function() {
         email: "ajayitaiwo89@gmail.com",
         password: "12345678"
       };
-      var authenticatedUser = request.agent(app);
-      authenticatedUser
-        .post("/signup")
+      // var authenticatedUser = request.agent(app);
+      server
+        .post("/login")
+        .expect("Content-type", /text/)
         .send(userCredentials)
         .end(function(error, response) {
           expect(response.statusCode).to.equal(200);
@@ -40,20 +47,23 @@ describe("Authentication", function() {
 
   describe("check login", function() {
     it("should return a 200 response if the user is logged in", function(done) {
-      request("http://localhost:4004/current-user", function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
+      server.get('/current-user')
+      .expect("Content-type",/text/)
+      .expect(200)
+      .end(function(err, res){
+        res.status.should.expect(200)
+        done()
+      })
     });
     it("should return a 302 response if the user is not logged in", function(done) {
-      request("http://localhost:4004/current-user", function(
-        error,
-        response,
-        body
-      ) {
-        expect(response.statusCode).to.equal(302);
-        done();
-      });
+      server
+        .get("/current-user")
+        .expect("Content-type", /text/)
+        .expect(302)
+        .end(function(err, res) {
+          res.status.should.expect(302);
+          done();
+        });
     });
   });
 });
